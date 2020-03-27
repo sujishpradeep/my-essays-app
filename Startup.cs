0a17a5cdb307fcf9735b50823582ae7a0c2e01b3
Sujish Pradeep <sujishp164@gmail.com>
2020-03-28 10:42:56 +1100

    Updated deployment specifics

--- Startup.cs
+++ Startup.cs
@@ -42,7 +42,7 @@ namespace my_essays_app
                 app.UseHsts();
             }
 
-            app.UseHttpsRedirection();
+            //  app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseSpaStaticFiles();
 
