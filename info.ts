1d498bda801516d21b3d9027290a0e0e6f454657
Sujish Pradeep <50685015+sujishpradeep@users.noreply.github.com>
2020-07-06 17:44:30 +1000

    Create info.ts

---
+++ info.ts
@@ -0,0 +1,37 @@
+<script type="text/javascript">
+      var _paq = window._paq || [];
+      /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+      _paq.push(["trackPageView"]);
+      _paq.push(["enableLinkTracking"]);
+      (function () {
+        var u = "https://betteron.matomo.cloud/";
+        _paq.push(["setTrackerUrl", u + "matomo.php"]);
+        _paq.push(["setSiteId", "1"]);
+        var d = document,
+          g = d.createElement("script"),
+          s = d.getElementsByTagName("script")[0];
+        g.type = "text/javascript";
+        g.async = true;
+        g.defer = true;
+        g.src = "//cdn.matomo.cloud/betteron.matomo.cloud/matomo.js";
+        s.parentNode.insertBefore(g, s);
+      })();
+      // window.addEventListener("hashchange", function () {
+      //   _paq.push(["setCustomUrl", "/" + window.location.hash.substr(1)]);
+      //   _paq.push(["setDocumentTitle", "My New Title"]);
+      //   _paq.push(["trackPageView"]);
+      // });
+    </script>
+    
+    
+    
+      let _paq = (window as any)._paq || [];
+    _paq.push(["setCustomUrl", window.location.href]);
+    _paq.push(["setDocumentTitle", "Specifiations"]);
+    _paq.push(["setUserId", user]);
+    _paq.push(["trackPageView"]);
+    
+    
+    let _paq = (window as any)._paq || [];
+    _paq.push(["trackEvent", "Paragraph", "Submitted", formValue.reference]);
+    _paq.push(["trackPageView"]);
